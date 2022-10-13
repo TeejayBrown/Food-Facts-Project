79b19b452de37163ed741f9119ab2b11372c446d
TeejayBrown <taiwo.omoleye@outlook.com>
2022-10-13 10:54:19 -0500

    Added ERB conditional to the Country and Product show views to provide additional information for Canadian products.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_201950) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_130817) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -19,14 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_201950) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "pages", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "permalink"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
